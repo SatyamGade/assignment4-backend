0a46aa8cffabb099f80e08441dd4c6e68f5c5212
SatyamGade <131511270+SatyamGade@users.noreply.github.com>
2024-03-21 10:30:04 +0530

    data imported in atlas

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-const URL = "mongodb://localhost:27017";
+const URL = "mongodb+srv://satyam:satya4426@cluster0.immfakx.mongodb.net/blackCoffer?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectDB = async ()=>{
     try {
-        await mongoose.connect(URL,{dbName: "blackCoffer"});
+        await mongoose.connect(URI);
         console.log("Database Connection Success...");
     } catch (error) {
         console.log("Database Connection Failed...");
